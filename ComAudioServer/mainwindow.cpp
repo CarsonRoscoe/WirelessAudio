6d672872245ba4aa32aa4502e56d8c960b8cae46
Spenser Lee <Spenser.L159@gmail.com>
2016-04-07 13:50:41 -0700

    server and client UI

--- ComAudioServer/mainwindow.cpp
+++ ComAudioServer/mainwindow.cpp
@@ -6,9 +6,15 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_startServerBtn_clicked()
+{
+
 }
--- ComAudioServer/mainwindow.h
+++ ComAudioServer/mainwindow.h
@@ -15,6 +15,10 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void on_startServerBtn_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
